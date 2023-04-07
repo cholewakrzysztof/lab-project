94db960d842d68d20d22aeafacf0c9a8ce991400
RafalUlikowski <rafalulikowski@gmail.com>
2023-04-08 01:50:13 +0200

    Fixed Clock

--- src/main/java/pl/edu/pwr/student/IO/Input/Clock.java
+++ src/main/java/pl/edu/pwr/student/IO/Input/Clock.java
@@ -6,7 +6,7 @@ public class Clock extends SignalSender implements Runnable {
     private final long intervalOn;
     private final long intervalOff;
 
-    private final Thread thread = new Thread(this);
+    private final Thread thread;
     private boolean power = false;
 
     public void toggle() {
@@ -20,16 +20,20 @@ public class Clock extends SignalSender implements Runnable {
 
     public void run() {
         while (power) {
-            Simulation.simWait(intervalOff);
             state = true;
-
+            sendUpdate();
             Simulation.simWait(intervalOn);
+
             state = false;
+            sendUpdate();
+            Simulation.simWait(intervalOff);
         }
     }
 
     public Clock(long millisecondsOn, long millisecondsOff) {
         intervalOn = millisecondsOn;
         intervalOff = millisecondsOff;
+
+        thread = new Thread(this);
     }
 }
