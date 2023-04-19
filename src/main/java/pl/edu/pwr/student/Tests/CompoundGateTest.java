14df0a9e248c2ed13e7eda8d8204e5c4a1c18359
cholewakrzysztof <cholewa.krzysztof24@gmail.com>
2023-04-19 02:57:43 +0200

    Add CompoundGateTest with SRLatch gate example, no tests yet

---
+++ src/main/java/pl/edu/pwr/student/Tests/CompoundGateTest.java
@@ -0,0 +1,41 @@
+package pl.edu.pwr.student.Tests;
+
+import junit.framework.TestCase;
+import pl.edu.pwr.student.Gates.BasicGates.NOR;
+import pl.edu.pwr.student.IO.Input.Switch;
+import pl.edu.pwr.student.IO.Output.LED;
+
+public class CompoundGateTest extends TestCase {
+	private Switch R = new Switch();
+	private Switch S = new Switch();
+	private LED out = new LED("SR Latch", 1000);
+	private NOR norUp = new NOR();
+	private NOR norDown = new NOR();
+	
+	/*
+	 * Create gate SRLatch from examples
+	 */
+	protected void setUp() {
+        R.connection(norUp);
+        S.connection(norDown);
+        norDown.connection(norUp);
+        norUp.connection(norDown);
+        norUp.connection(out);
+	}
+	
+	/*
+	 * Test gate work input 1 and 1
+	 */
+	public void testWorkCase1() {
+		R.toggle();
+		S.toggle();
+		assertTrue(out.getState());
+	}
+	/*
+	 * Test gate work input 1 and 1
+	 */
+	public void testWorkCase2() {
+		S.toggle();
+		assertTrue(out.getState());
+	}
+}
