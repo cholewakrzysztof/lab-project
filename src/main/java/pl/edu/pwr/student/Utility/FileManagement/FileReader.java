24c594e47d4de0016722f4a701dfc75ba0c7a1e1
cholewakrzysztof <cholewa.krzysztof24@gmail.com>
2023-05-04 15:05:49 +0200

    Set up new branch

---
+++ src/main/java/pl/edu/pwr/student/Utility/FileManagement/FileReader.java
@@ -0,0 +1,4 @@
+package pl.edu.pwr.student.Utility.FileManagement;
+
+public class FileReader {
+}
---
+++ src/main/java/pl/edu/pwr/student/Utility/FileManagement/FileWriter.java
@@ -0,0 +1,11 @@
+package pl.edu.pwr.student.Utility.FileManagement;
+
+public class FileWriter {
+    /**
+     * U
+     */
+    public static void safeToFile(){
+
+    }
+
+}
