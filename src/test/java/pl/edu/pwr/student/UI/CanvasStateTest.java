1918538b73538659f679e73966fd241d054f2bd0
cholewakrzysztof <cholewa.krzysztof24@gmail.com>
2023-06-19 17:14:32 +0200

    Add test to CanvasState

--- src/test/java/pl/edu/pwr/student/UI/CanvasStateTest.java
+++ src/test/java/pl/edu/pwr/student/UI/CanvasStateTest.java
@@ -1,26 +1,44 @@
 package pl.edu.pwr.student.UI;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CanvasStateTest {
 
+    /**
+     * Set CanvasState to default configuration
+     */
+    @BeforeEach
+    void setCanvasState(){
+        CanvasState.setState(CanvasState.State.INTERACTING);
+    }
+
+    /**
+     * Test changing state
+     */
     @Test
     void setState() {
-        CanvasState.setState(1);
-        assertTrue(1==CanvasState.getState());
+        CanvasState.setState(CanvasState.State.OUTPUT);
+        assertTrue(CanvasState.State.OUTPUT==CanvasState.getState());
     }
 
+    /**
+     * Test getting state
+     */
     @Test
     void getState() {
-        assertTrue(0==CanvasState.getState());
+        assertTrue(CanvasState.State.INTERACTING==CanvasState.getState());
     }
 
+    /**
+     * Test setting last state
+     */
     @Test
     void lastState() {
-        CanvasState.setState(1);
+        CanvasState.setState(CanvasState.State.OUTPUT);
         CanvasState.lastState();
-        assertTrue(0==CanvasState.getState());
+        assertTrue(CanvasState.State.INTERACTING==CanvasState.getState());
     }
 }
